94088f0460aa6e335856fcb5ce2180bb06e673e3
Nick-Slaz <nslazins@gmail.com>
2026-01-28 10:01:26 -0500

    Discussion

--- Discussion3.py
+++ Discussion3.py
@@ -3,19 +3,23 @@ import math
 class Rectangle():
     # Create the constructor "__init__" method
 
-    # YOUR CODE HERE
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
 
 
 
     # Create the "__str__" method
 
-    # YOUR CODE HERE
+    def __str__(self):
+        return f"A rectangle with width {self.width} and height {self.height}"
 
 
 
     # Create the "area_calculator" method
 
-    # YOUR CODE HERE
+    def area_calculator(self):
+        return float(self.width * self.height)
 
 
 
@@ -23,7 +27,11 @@ class Rectangle():
     # 
     # Returns a boolean value
 
-    # YOUR CODE HERE
+    def __eq__(self, other):
+        if self.width == other.width and self.height == other.height
+            return True
+        else:
+            return False
 
 
     
